aee8ac3e5691215e7eae0f89dc247a549adbd5be
Crazy-Cli1k <dima.samik.1506@gmail.com>
2022-01-31 11:59:30 +0500

    master

---
+++ Calculator/Program.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Введите A");
+double A = Double.Parse(Console.ReadLine());
+Console.WriteLine("Введите B");
+double B = Double.Parse(Console.ReadLine());
+
+Console.WriteLine("Выберите функцию:\n1) - выполнить операцию “+”\n2) - выполнить операцию “-”\n3) - выполнить операцию “*”\n4) - выполнить операцию “/”");
+int choice = Int32.Parse(Console.ReadLine());
+
+if (choice == 1)
+{
+
+}
+else if (choice == 2)
+{
+
+}
+else if (choice == 3)
+{
+
+}
+else if (choice == 4)
+{
+
+}
+else Console.WriteLine("Ошибка! Неверные данные!");
